8c251686262456b421afffa1c32ab816ffaf04d4
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-09 14:02:57 +0700

    feat: update in tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(() => {
-    const savedCount = localStorage.getItem('angka');
-    return savedCount !== null ? parseInt(savedCount) : 0;
-  });
-
-  const handleClick = () => {
-    setCount((prevCount) => {
-      const newCount = prevCount + 1;
-      localStorage.setItem('angka', newCount);
-      return newCount;
-    });
-  };
-
-  const handleRemove = () => {
-    setCount(0)
-    localStorage.setItem('angka', 0)
-  };
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
-        <br />
-        <br />
-        <button onClick={handleRemove}>Clear Count</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div className="text-5xl text-blue-800">Hello World</div>
+      <div className='text-black bg-red-600'>yuhu</div>
     </>
   );
 }
