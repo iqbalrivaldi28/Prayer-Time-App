8fbd88a30d56a6faaa945292783a728e9e5f17d0
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-09 15:21:43 +0700

    ref: refactor day and time with dapa from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [input, setInput] = useState('');
+  const [term, setTerm] = useState('Jakarta');
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`http://api.aladhan.com/v1/timingsByAddress?address=${term}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.data));
+  }, [term]);
+
   return (
     <>
       <div className="h-screen flex flex-col bg-[url('./assets/bg-masjid.jpg')] bg-cover  text-white gap-5 justify-center items-center">
         <div className="flex flex-col w-full justify-between items-center gap-3 absolute top-5">
           <div className="w-full flex justify-between px-[70px] text-[#e3e6e3]">
             <div>
-              <div>Wednesday</div>
-              <div>9 Juni 2024</div>
+              <div>{data.date && data.date.gregorian.weekday.en}</div>
+              <div>{data.date && data.date.readable}</div>
             </div>
             <div>Jakarta</div>
           </div>
-          <form>
-            <input type="text" placeholder="Search Prayer Time by City" className="w-[250px] py-2 px-4 rounded-full text-[14px] text-slate-400 outline-none focus:shadow-lg" />
+          <form onSubmit={(e) => {
+              e.preventDefault();
+              setTerm(input)
+              setInput('')
+          }}>
+            <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Search Prayer Time by City" className="w-[250px] py-2 px-4 rounded-full text-[14px] text-slate-400 outline-none focus:shadow-lg" />
             <input type="submit" value="" />
           </form>
         </div>
@@ -39,8 +55,6 @@ function App() {
             </li>
           </ul>
         </div>
-
-
       </div>
     </>
   );
